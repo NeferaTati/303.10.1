e2591c4af3eb3f2a3f0d655934e68a6951683f03
NeferaTati <113719752+NeferaTati@users.noreply.github.com>
2024-09-04 10:22:28 -0500

    Add files via upload

---
+++ Boy.java
@@ -0,0 +1,15 @@
+public class Boy extends Person {
+    static double   ageFactor = 1.1;
+
+    public String talk() {
+        return(super.talk() + " .G.. but I love Java class.");
+    }
+    public String walk() {
+        return("I am now walking");
+    }
+}
+
+
+
+
+
